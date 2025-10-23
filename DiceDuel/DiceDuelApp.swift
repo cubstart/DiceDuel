e74ceb17d09307095c5939bfc93ca98b68292d8e
justinwongeecs <jwonghelloworld@berkeley.edu>
2025-10-22 20:01:11 -0700

    Initial Commit

---
+++ DiceDuel/DiceDuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceDuelApp.swift
+//  DiceDuel
+//
+//  Created by Justin Wong on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceDuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
